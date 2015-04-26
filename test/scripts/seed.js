19717ec68817e83d91867e5b43d44ea3eef297e3
Michael Lai <themichaellai@gmail.com>
2015-04-25 23:12:54 -0400

    Added template seed script

---
+++ test/scripts/seed.js
@@ -0,0 +1,29 @@
+var lorem = require('lorem-ipsum');
+var db = require('monk')(process.env.MONGOLAB_URI || "mongodb://localhost/dukeexchange");
+var _ = require('underscore');
+var async = require('async');
+
+var listings = db.get('listings');
+var data = _.times(10, function(n) {
+  return {
+    title: lorem({
+      count: 3,
+      units: 'words',
+    }) + ' ' + n,
+    createdAt: new Date(),
+    images: []
+  };
+});
+
+async.parallel(data.map(function(d) {
+  return function(cb) {
+    listings.insert(d, cb);
+  };
+}), function(err, res) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+    return;
+  }
+  process.exit(0);
+});
